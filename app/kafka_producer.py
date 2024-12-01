aae0ecc71d83945c89427ef0697adfc3f1830c8f
Max N <maxwell.evans22@gmail.com>
2024-12-01 01:34:41 -0800

    added apt upgrade to docker playbook; implemented ch get client and get engine; broke out kafka consumer and redis client creation; began ch load logic; added growing sleep to kafka producer

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -8,12 +8,13 @@ def main():
     config_ = load_config(file_name='kafka_producer_config.toml')
     producer = Producer(config_['kafka_broker'])
 
+    sleep_seconds = 1
     while True:
-        print(f'topics: {producer.list_topics().topics}')
-        val_ = str(random.randint(0, 100)/100)
+        val_ = str(random.randint(-100, 100))
         producer.produce(config_['kafka']['topic'], key=config_['kafka']['key'], value=val_)
-        print(f'pushed: {val_}')
-        time.sleep(1)
+        time.sleep(sleep_seconds)
+        sleep_seconds = round(sleep_seconds * 1.01, 2)
+        print(sleep_seconds)
 
 
 if __name__ == '__main__':
