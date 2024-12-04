8d59b31a3f2ad11fa3f68159a4f75a9f0124ca89
Max N <maxwell.evans22@gmail.com>
2024-12-04 01:54:11 -0800

    updated clickhouse playbook to copy modified config allowing external connections; updated load_config function to point to newly created app_configs dir

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,7 @@ import os
 def load_config(file_name):
     # assumes config is in project configs dir
     curr_path = os.path.dirname(os.path.realpath(__file__))
-    producer_config_path = f'{curr_path}/../configs/{file_name}'
+    producer_config_path = f'{curr_path}/../configs/app_configs/{file_name}'
     config_ = toml.load(producer_config_path)
 
     return config_
